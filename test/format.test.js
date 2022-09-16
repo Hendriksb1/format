dea41f8c89044f00b85df582da0d1ecb6294005b
hendriksb1 <hendrik.schulze.boeing@gmail.com>
2022-09-16 15:32:37 +0200

    removed commented out code

--- test/format.test.js
+++ test/format.test.js
@@ -1,4 +1,3 @@
-//const assert = require('assert');
 import assert from 'node:assert/strict';
 import {format} from '../utils/format.js'
 
@@ -24,43 +23,7 @@ describe('simple Test', () => {
     it('-1,5555 should return true', () => {
         assert.equal("-1,5555", format("-1.55555", 4));
     });
-
-
-
     it('1,555 should return true', () => {
         assert.equal("1,555", format(1.55555, 3, ",", ".", false));
     });
-
-
-    it('1,56 should return true', () => {
-        assert.equal("1,56", format("1.5555", 2));
-    });
 });
-
-
-
-
-
-
-
-
-
-
-
-// let format = function (val = 0, fractionDigits = 2, decimalSeperator = ",", readabilitySeperator = ".") {
-//
-//     const defaultLocale = "de-DE";
-//     const alternateLocale = "en-US";
-//
-//
-//     let result = val.toLocaleString(defaultLocale, { maximumFractionDigits: fractionDigits, minimumFractionDigits: fractionDigits })
-//
-//     if (decimalSeperator === "." && readabilitySeperator === ",") {
-//         result = val.toLocaleString(alternateLocale, { maximumFractionDigits: fractionDigits, minimumFractionDigits: fractionDigits });
-//     }
-//
-//     console.log(result)
-//
-//
-//     return result
-// }
