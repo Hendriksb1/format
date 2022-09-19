92f69da1d608fad25ab1f66871c5d2c7791a40d0
hendriksb1 <hendrik.schulze.boeing@gmail.com>
2022-09-19 22:13:41 +0200

    formatting the code

--- utils/format.js
+++ utils/format.js
@@ -1,7 +1,7 @@
 // returns a formatted string 
 // input val can be of type number or string
 // currently supporting de-DE by default and en-US is optional
-export function format(val = 0, fractionDigits = 2, decimalSeperator = ",", thousandsSeparator = ".", flag= true) {
+export function format(val = 0, fractionDigits = 2, decimalSeperator = ",", thousandsSeparator = ".", flag = true) {
 
     // ensure it is a number (floating-point)
     if (typeof val === 'string') {
@@ -9,10 +9,13 @@ export function format(val = 0, fractionDigits = 2, decimalSeperator = ",", thou
     }
 
     let formatToLocale = function (locale) {
-        return new Intl.NumberFormat(locale, { minimumFractionDigits: fractionDigits, maximumFractionDigits: fractionDigits }).format(val);
+        return new Intl.NumberFormat(locale, {
+            minimumFractionDigits: fractionDigits,
+            maximumFractionDigits: fractionDigits
+        }).format(val);
     }
 
-    let result = formatToLocale( 'de-DE');
+    let result = formatToLocale('de-DE');
 
     if (decimalSeperator === "." && thousandsSeparator === ",") {
         result = formatToLocale('en-US');
@@ -30,8 +33,7 @@ export function format(val = 0, fractionDigits = 2, decimalSeperator = ",", thou
     return result
 }
 
-
 let newLength = function (number, fractionDigits) {
     let i = number.toString().indexOf('.');
-    return  number.toString().substring(0, i).length + fractionDigits + 1
+    return number.toString().substring(0, i).length + fractionDigits + 1
 }
