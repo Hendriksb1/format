bbf66555c92954ebfa9ece17e240e441a74d4d69
Hendrik Schulze Böing <36677329+Hendriksb1@users.noreply.github.com>
2022-09-19 18:28:10 +0200

    Update format.js

--- utils/format.js
+++ utils/format.js
@@ -1,6 +1,6 @@
-// returns a formatted entry value
-// val can be of type number or string
-// can be configured to use a specific locale ( currently supporting de-De and en-US )
+// returns a formatted string 
+// input val can be of type number or string
+// currently supporting de-De and en-US 
 export function format(val = 0, fractionDigits = 2, decimalSeperator = ",", thousandsSeparator = ".", flag= true) {
 
     if (typeof val === 'string') {
